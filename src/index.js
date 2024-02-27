38e357a72dc7774cf3432f134e5b7d2f3b7801c4
baptiste66 <101480985+baptiste66@users.noreply.github.com>
2024-02-27 22:19:04 +0100

    arborescence + route

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './Styles/index.css';
+import Home from './pages/Home';
+import User from './pages/User';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/user/:id",
+    element: <User />,
+  },
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
